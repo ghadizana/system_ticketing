a3994d527089bf1bc6af3aecda6f5a9e2600257d
ghadizana <ghadizanauraaliya@gmail.com>
2024-07-11 16:56:39 +0700

    add stack for script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,6 +87,7 @@
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
+    @stack('scripts')
 </body>
 
 
